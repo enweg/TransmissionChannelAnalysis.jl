e27edf958557bd3ed9b2ad41eb29afc6610ac718
Enrico Wegner <wegner1996@gmail.com>
2023-10-23 11:57:30 +0200

    exporting library functions

--- src/TransmissionMechanisms.jl
+++ src/TransmissionMechanisms.jl
@@ -9,5 +9,11 @@ include("./rules.jl")
 include("./simplifying.jl")
 include("./condition.jl")
 include("./transmission-function.jl")
+include("./structural-representation.jl")
+
+export make_condition
+export create_transmission_function
+export make_structural_B, make_structural_Qbb
+export to_structural_transmission_model
 
 end

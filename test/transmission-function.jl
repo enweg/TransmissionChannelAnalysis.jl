af21d2592a1e4b550e080f793747659e78d55ae1
Enrico Wegner <wegner1996@gmail.com>
2024-02-02 11:16:08 +0100

    IRF method does always work, even for the first few. So do not show NaNs

--- test/transmission-function.jl
+++ test/transmission-function.jl
@@ -169,36 +169,42 @@ end
     Qbb = read_json_to_matrix("./simulated-svar-k3-p1/Qbb.json")
 
     mat = test1(irfs, irfs_ortho, B, Qbb)
+    @test isapprox(mat[:, 1], mat[:, 4]; atol = sqrt(eps()))
     mat = mat[3:end, :]
     @test isapprox(mat[:, 1], mat[:, 3]; atol = sqrt(eps()))
     @test isapprox(mat[:, 1], mat[:, 2]; atol = sqrt(eps()))
     @test isapprox(mat[:, 1], mat[:, 4]; atol = sqrt(eps()))
 
     mat = test2(irfs, irfs_ortho, B, Qbb)
+    @test isapprox(mat[:, 1], mat[:, 4]; atol = sqrt(eps()))
     mat = mat[4:end, :]    
     @test isapprox(mat[:, 1], mat[:, 3]; atol = sqrt(eps()))
     @test isapprox(mat[:, 1], mat[:, 2]; atol = sqrt(eps()))
     @test isapprox(mat[:, 1], mat[:, 4]; atol = sqrt(eps()))
 
     mat = test3(irfs, irfs_ortho, B, Qbb)
+    @test isapprox(mat[:, 1], mat[:, 4]; atol = sqrt(eps()))
     mat = mat[3:end, :]
     @test isapprox(mat[:, 1], mat[:, 3]; atol = sqrt(eps()))
     @test isapprox(mat[:, 1], mat[:, 2]; atol = sqrt(eps()))
     @test isapprox(mat[:, 1], mat[:, 4]; atol = sqrt(eps()))
 
     mat = test4(irfs, irfs_ortho, B, Qbb)
+    @test isapprox(mat[:, 1], mat[:, 4]; atol = sqrt(eps()))
     mat = mat[4:end, :]
     @test isapprox(mat[:, 1], mat[:, 3]; atol = sqrt(eps()))
     @test isapprox(mat[:, 1], mat[:, 2]; atol = sqrt(eps()))
     @test isapprox(mat[:, 1], mat[:, 4]; atol = sqrt(eps()))
 
     mat = test5(irfs, irfs_ortho, B, Qbb)
+    @test isapprox(mat[:, 1], mat[:, 4]; atol = sqrt(eps()))
     mat = mat[6:end, :]
     @test isapprox(mat[:, 1], mat[:, 3]; atol = sqrt(eps()))
     @test isapprox(mat[:, 1], mat[:, 2]; atol = sqrt(eps()))
     @test isapprox(mat[:, 1], mat[:, 4]; atol = sqrt(eps()))
 
     mat = test6(irfs, irfs_ortho, B, Qbb)
+    @test isapprox(mat[:, 1], mat[:, 4]; atol = sqrt(eps()))
     mat = mat[6:end, :]
     @test isapprox(mat[:, 1], mat[:, 3]; atol = sqrt(eps()))
     @test isapprox(mat[:, 1], mat[:, 4]; atol = sqrt(eps()))

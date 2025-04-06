454197980ba40a3bf14266d68c683f9126fac0a0
Enrico Wegner <wegner1996@gmail.com>
2025-04-06 14:17:37 +0200

    IRF pretty print + utilities

--- src/models/tools.jl
+++ src/models/tools.jl
@@ -14,6 +14,32 @@ function IRF(
     return IRF(irfs, varnames, model, nothing)
 end
 
+function Base.show(io::IO, mime::MIME"text/plain", x::IRF)
+    return Base.show(io, mime, x.irfs)
+end
+
+Base.getindex(x::IRF, inds...) = x.irfs[inds...]
+Base.:+(x::IRF, y) = x.irfs + y
+Base.:+(x::IRF, y::Number) = x.irfs .+ y
+Base.:+(x, y::IRF) = x + y.irfs
+Base.:+(x::Number, y::IRF) = x .+ y.irfs
+Base.:-(x::IRF, y::Number) = x.irfs .- y 
+Base.:-(x::IRF, y) = x.irfs - y 
+Base.:-(x, y::IRF) = x - y.irfs
+Base.:-(x::Number, y::IRF) = x .- y.irfs
+Base.:*(x::IRF, y) = x.irfs * y
+Base.:*(x::IRF, y::Number) = x.irfs .* y
+Base.:*(x, y::IRF) = x * y.irfs
+Base.:*(x::Number, y::IRF) = x .* y.irfs
+Base.:/(x::IRF, y) = x.irfs / y
+Base.:/(x::IRF, y::Number) = x.irfs ./ y
+Base.:/(x, y::IRF) = x / y.irfs
+Base.:/(x::Number, y::IRF) = x ./ y.irfs
+Base.size(x::IRF) = size(x.irfs)
+Base.length(x::IRF) = length(x.irfs)
+
+
+
 #-------------------------------------------------------------------------------
 # API DESIGNS
 #-------------------------------------------------------------------------------

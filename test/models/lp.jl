0b435e8915ee3e918646f484077e1dd840f25500
Enrico Wegner <wegner1996@gmail.com>
2025-04-18 16:40:56 +0200

    fixing test

--- test/models/lp.jl
+++ test/models/lp.jl
@@ -9,7 +9,7 @@ using Random
     T = 10
     p = 2
 
-    data = DataFrame(reshape(repeat(Float64.(1:T), k), :, k), :auto)
+    data = DataFrame(reshape(1:(T*k), T, k), :auto)
 
     treatment = 1
     horizons = 0:3
@@ -19,7 +19,7 @@ using Random
     @test all(model.X[:, 1] .== 1)
     # testing the rest of the matrices
     for (i, h) in enumerate(horizons)
-        @test isequal(model.X[1:(end-h), 2], model.Y[1:(end-h), 2, i] .- h)
+        @test isequal(model.X[1:(end-h), 2:(treatment+1)], model.Y[1:(end-h), 1:treatment, i] .- h)
         for j = 1:p
             @test isequal(model.X[1:(end-h), ((j-1)*k+3):(j*k+2)], model.Y[1:(end-h), :, i] .- h .- j)
         end

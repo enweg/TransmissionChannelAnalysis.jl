be192af4a800154bbb5f15669ce42b7b1d12c383
Enrico Wegner <wegner1996@gmail.com>
2025-06-18 10:21:20 +0200

    deleting old implementation of DFM

--- src/models/dfm/abstract.jl
+++
@@ -1,1 +0,0 @@
-abstract type DFM <: Model end
--- src/models/dfm/utils.jl
+++
@@ -1,8 +0,0 @@
-"""
-Convert a lag polynomial `[X_1, X_2, X_3, ...]` to a matrix that 
-stacks the lag-matrices horizontally so that it returns `[X_1 X_2 ... ]`
-"""
-function lag_poly_to_matrix(lag_poly::AbstractVector{<:AbstractMatrix})
-    return reduce(hcat, lag_poly)
-end
-
